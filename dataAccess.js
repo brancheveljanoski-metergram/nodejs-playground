265c9df32e02630baf10565557fabfb441d12b42
Nikola Jankov <nikola.jankov@metergram.com>
2022-04-28 13:31:15 +0200

    Added JSON data access functionality

---
+++ dataAccess.js
@@ -0,0 +1,133 @@
+const { json } = require('express/lib/response');
+const fs = require('fs');
+
+let path = './movies.json';
+const rawData = fs.readFileSync(path);
+let movies = JSON.parse(rawData);
+
+
+//---------------------JSON Reading-----------------------
+
+function getAll(){
+    return JSON.stringify(movies);
+}
+
+function getByID(ID){
+   const movie = movies.find(movie => movie.imdbID == ID);
+   return JSON.stringify(movie);
+}
+
+function getByGenre(genre){
+    const genreMovies = movies.filter(movie =>movie.Genre.includes(genre));
+    return JSON.stringify(genreMovies);
+}
+
+function getByActor(actor){
+    const actorMovies = movies.filter(movie =>movie.Actors.includes(actor));
+    return JSON.stringify(actorMovies);
+}
+
+function sortByRating(isASC){
+    let sortedMovies;
+
+    if(isASC){
+        sortedMovies=movies
+        .sort((movieA, movieB) => movieA.imdbRating - movieB.imdbRating)
+    }else{
+        sortedMovies=movies
+        .sort((movieA, movieB) => movieB.imdbRating - movieA.imdbRating)
+    }
+    
+    const movieTitleAndRating = sortedMovies
+    .map(m => {return {Title: m.Title, imdbRating: m.imdbRating}})
+
+    return JSON.stringify(movieTitleAndRating);
+}
+
+function totalLengthOfMovies(){
+    const totalLen = movies.reduce((total, movie)=> {
+        const movieLen= parseInt(movie.Runtime);
+        return !isNaN(movieLen) ? movieLen + total : total;
+    }, 0)
+
+    return totalLen;
+}
+
+function getUrls(){
+    const urls = movies.map(movie => {
+        return `https://www.imdb.com/title/${movie.imdbID}/`;
+    });
+
+    return JSON.stringify(urls);
+}
+
+
+function totalImdbVotes(){
+    const totalVotes = movies.reduce((total, movie) => {
+        const movieVotes = parseInt(movie.imdbVotes.replace(/,/g, ''));
+        return !isNaN(movieVotes) ? movieVotes + total : total;
+    }, 0);
+
+    return totalVotes;
+}
+
+function allLanguagues(){
+    const allLanguages = movies.reduce((languages, movie)=> {
+        return languages.concat(movie.Language.split(", "));
+    }, languages = [])
+
+    return JSON.stringify([...new Set(allLanguages)]);
+}
+
+
+//----------------------JSON Writing----------------
+
+function writeToJSON(data){
+    fs.writeFile(path, JSON.stringify(data, null, "\t"), (err)=>{
+        if(err) return false;
+    });
+
+    return true;
+}
+
+function addMovie(newMovie){
+    if(getByID(newMovie.imdbID) != undefined){
+        console.log(false);
+        return false;
+    }
+    movies.push(newMovie);
+    
+    if(writeToJSON(movies)) {
+        return true
+    };
+    
+    return false;
+    
+}
+
+
+function editMovie(editedMovie){
+    const movieIndex = movies.findIndex(movie => movie.imdbID == editedMovie.imdbID);
+    movies[movieIndex] = editedMovie;
+
+    if(writeToJSON(movies)) {
+        return true
+    };
+
+    return false;
+}
+
+function deleteMovie(movieID){
+    const movieIndex = movies.findIndex(movie => movie.imdbID == movieID);
+    if(movieIndex != -1){
+        movies.splice(movieIndex, 1);
+        if(writeToJSON(movies)) {
+            return true
+        };
+
+        return false;
+    }
+
+    return false;
+}
+
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("SUP");
+});
+
+app.post('/', (req, res)=>{
+    console.log(req);
+    res.send('Got a POST req');
+});
+
+app.put('/user', (req, res)=>{
+    console.log('Got PUT for user'+ req);
+    res.send("Got a PUT req");
+});
+
+app.delete('/user', (req, res)=>{
+    console.log('Got DELETE for user'+ req);
+    res.send("Got a DELETE req");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server on port ${port}`);
+})
