ba02c98477fb01063e0d0fc975415dca773d0405
Tijana Trojnko <tijana.trojnko@metergram.com>
2022-05-01 22:30:00 +0200

    filtering & get commands

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const movies = require('./movieManipulation');
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+app.get('/movies/', (req, res) => {
+    res.json(movies.getAllMovies());
+});
+
+app.get('/movies/query', (req, res)=>{
+    const {genre, actor, imdbSort} = req.query;
+    let allMovies = movies.getAllMovies();
+    if(genre)
+        allMovies = allMovies.filter(m =>m.Genre.includes(genre));
+
+    if(actor)
+        allMovies = allMovies.filter(m =>m.Actors.includes(actor));
+
+    if(imdbSort){
+        if(imdbSort === 'DESC'||'desc')
+            allMovies = movies.sortByRating(true, allMovies)
+        else if(imdbSort === 'ASC'||'asc')
+            allMovies = movies.sortByRating(false, allMovies)
+    }
+    res.json(allMovies);
+});
+
+app.get('/movies/:imdbID', (req, res)=>{
+    const id = req.params.imdbID;
+    const movie = movies.getSpecificMovie(id);
+    res.json(movie);
+});
+
+app.get('/movies/information/length', (req, res)=>{
+    res.json(`${movies.totalLengthOfAllMovies()}`);
+});
+
+app.get('/movies/information/languages', (req, res)=>{
+    res.json(movies.allLanguagues());
+});
+
+app.get('/movies/information/imdbUrls', (req, res)=>{
+    res.json(movies.imdbUrls());
+});
+
+app.get('/movies/information/votes', (req, res)=>{
+    res.json(movies.totalImdbVotes());
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`);
+})
---
+++ movieManipulation.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+let file = './movies.json';
+const moviefile = fs.readFileSync(file);
+let movies = JSON.parse(moviefile);
+
+function getAllMovies(){
+    return movies;
+}
+
+function filterByGenre(genre){
+    const genreMovies = movies.filter(movie =>movie.Genre.includes(genre));
+    genreMovies;
+}
+
+function filterByActor(actor){
+    const actorMovies = movies.filter(movie =>movie.Actors.includes(actor));
+    actorMovies;
+}
+
+function sortByRating(isDESC,allMovies){
+    let result;
+    if(isDESC){
+        result=allMovies.sort((x, y) => y.imdbRating - x.imdbRating)
+    }else{
+        result=allMovies.sort((x, y) => x.imdbRating - y.imdbRating)
+    }
+    const resultFinal = result.map(m => {return {Title: m.Title, imdbRating: m.imdbRating}})
+    return resultFinal;
+}
+
+function getSpecificMovie(ID){
+    const movie = movies.find(movie => movie.imdbID == ID);
+    return movie;
+}
+
+function totalLengthOfAllMovies(){
+    const result = movies.reduce((sum, movie)=> {
+        const length = parseInt(movie.Runtime);
+        return length ? length + sum : sum;
+    }, 0)
+    var num = result;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return rhours + " hours and " + rminutes + " minutes.";
+}
+
+function imdbUrls(){
+    const imdburls = movies.map(movie => {
+        return `https://www.imdb.com/title/${movie.imdbID}/`;
+    });
+    return imdburls;
+}
+
+function totalImdbVotes(){
+    const result = movies.reduce((sum, movie) => {
+        const votes = parseInt(movie.imdbVotes.replace(/,/g, ''));
+        return votes ? votes + sum : sum;
+    }, 0);
+
+    return result;
+}
+
+function onlyUnique(allLanguages, index, self) { 
+    return self.indexOf(allLanguages) === index;
+}
+
+function allLanguagues(){
+    const allLanguages = movies.reduce((languages, movie)=> {
+        return languages.concat(movie.Language.split(", "));
+    }, languages = [])
+    var unique = allLanguages.filter(onlyUnique);
+    return unique;
+}
+
+module.exports = {getAllMovies, filterByActor, filterByGenre, imdbUrls, sortByRating, getSpecificMovie, totalImdbVotes, totalLengthOfAllMovies, allLanguagues};
