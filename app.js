330f76f6d9eb3c43420d493614eec95fb0395742
Viktorija Matejevik <viktorija.matejevik@metergram.com>
2022-05-03 09:15:11 +0200

    Second commit

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const movies = require('./movies.json');
+
+
+//Gets All Movies 
+app.get('/movies', (req,res)=>{
+    res.json(movies);
+})
+
+
+//Filter Movies By Genre
+app.get('/movies/:genre',(req,res)=>{
+    res.json(movies.filter(movie => 
+        movie.Genre.includes(req.params.genre)))
+})
+
+
+//Filter Movies By Actor                                                                                            
+app.get('/movies/:actor',(req,res)=>{
+    const found= movies.some(movie=>movie.Actors.includes(req.params.actor))
+
+    if(found){
+        res.json(movies.filter(movie => 
+            movie.Actors.includes(req.params.actor)))
+    }else{
+        res.status(400).json({msg: `No movie with the actor name of ${req.params.actor}`})
+    }
+})
+
+
+//Sort Movies By IMDB rating
+app.get('/movies/sort',(req,res)=>{
+
+    let sortedMovies;
+    sortedMovies=movies.sort((movie1,movie2)=>movie1.imdbRating-movie2.imdbRating)
+    //res.json(movies.sort((movie1,movie2)=>movie1.imdbRating-movie2.imdbRating))
+
+    const movieTitleAndRating = sortedMovies.map(movie=>{return{Title: movie.Title, imdbRating: movie.imdbRating}})
+
+    res.json(movieTitleAndRating);
+
+
+})
+
+
+//Get Specific Movie By Rating
+app.get('/movies/:rating',(req,res)=>{
+    res.json(movies.filter(movie => 
+        movie.imdbRating == req.params.rating))
+})
+
+
+//Body Parses Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+//Add New Movie
+app.post('/movies',(req,res)=>{
+    const newMovie= {
+        title: req.body.Title,
+        genre: req.body.Genre
+    }
+    if(!newMovie.title || !newMovie.genre){
+        return res.status(400).json({msg: 'Please include a title and genre'})
+    }
+
+    movies.push(newMovie)
+    res.json(movies)
+})
+
+
+//Edit Existing Movie
+app.put('/movies/:title',(req,res)=>{
+    const found= movies.some(movie=>movie.Title === req.params.title)
+
+    if(found){
+        const updMovie = req.body;
+        movies.forEach(movie =>{
+            if(movie.Title === req.params.title){
+                movie.Title = updMovie.Title ? updMovie.Title : movie.Title;
+                movie.Genre = updMovie.Genre ? updMovie.Genre : movie.Genre;
+
+                res.json({msg: 'Movie updated', movie})
+            }
+        })
+    }else{
+        res.status(400).json({msg: `No movie with the actor name of ${req.params.title}`})
+    }
+})
+
+
+//Delete Existing Movie
+app.delete('/movies/:title',(req,res)=>{
+    const found= movies.some(movie=>movie.Title === req.params.title)
+
+    if(found){
+        res.json({msg: 'Movie deleted', movies: movies.filter(movie => 
+            movie.Title !== req.params.title)})
+    }else{
+        res.status(400).json({msg: `No movie with the title name of ${req.params.title}`})
+    }
+})
+
+app.listen(1000,()=>{
+    console.log('server is listening on port 10000...')
+})
