8637f4abc8c2ab34cd456f49c2963ab3487ad207
viktorijamatejevik-metergram <104424659+viktorijamatejevik-metergram@users.noreply.github.com>
2022-05-06 12:40:59 +0200

    Create error-handler.js

---
+++ src/lib/error-handler.js
@@ -0,0 +1,49 @@
+const HTTP = require("http-status-codes");
+
+/**
+ * Wraps a request handler or middleware in a way that should ensure that any errors are propagated correctly to the caller.
+ * @param handler the handler to wrap. Must return a promise.
+ * @return {Function} the wrapped handler function.
+ */
+function wrap(handler) {
+    return async (req, res) => {
+        try {
+            const result = await handler(req, res);
+            const responseBody = JSON.stringify(result);
+            res.set({
+                "Content-Type": "application/json",
+                "Content-Length": responseBody.length
+            });
+            res.status(HTTP.StatusCodes.OK);
+            return res.send(result);
+        } catch (err) {
+            return getErrorHandler(res, err);
+        }
+    };
+}
+
+/**
+ * Generates an error handler for a response.
+ * @param res the response to use to propagate the error.
+ * @return {Function} the error handler function.
+ */
+function getErrorHandler(res, err) {
+    let code = parseInt(err.statusCode || err.code);
+    if (typeof code !== "number" || isNaN(code)) {
+
+        code = HTTP.StatusCodes.INTERNAL_SERVER_ERROR;
+        console.error([
+            "Unexpected error thrown",
+            "Code 500",
+            `Message ${err.message}`,
+            `Stack ${err.stack}`,
+            `Raw ${err}`,
+        ].join("\n"));
+    }
+    res.status(code);
+    res.send(err);
+}
+
+module.exports = {
+    wrap
+};
